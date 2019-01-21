3f68c26fc9e31c57824abbc6833a6111b962f5b9
racsoraul <racsoraul@gmail.com>
2019-01-21 13:22:34 -0600

    Double quote to prevent globbing and word splitting.

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Generating Simbotic project files"
 echo "----------------------"
-$SIMBOTIC_UE4/GenerateProjectFiles.sh -project=$SIMBOTIC_ROOT/Simbotic.uproject -game -engine -Makefile -vscode
+"$SIMBOTIC_UE4"/GenerateProjectFiles.sh -project="$SIMBOTIC_ROOT"/Simbotic.uproject -game -engine -Makefile -vscode
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-$SIMBOTIC_UE4/Engine/Binaries/Linux/UE4Editor $SIMBOTIC_ROOT/Simbotic.uproject
+"$SIMBOTIC_UE4"/Engine/Binaries/Linux/UE4Editor "$SIMBOTIC_ROOT"/Simbotic.uproject
