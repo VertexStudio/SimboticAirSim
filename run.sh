cdefac30f83d45546428ded6c37304a9559dcb9f
racsoraul <racsoraul@gmail.com>
2019-01-22 11:33:43 -0600

    Modified 'run.sh' to take the 'airsim' command line argument.

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-"$SIMBOTIC_UE4"/Engine/Binaries/Linux/UE4Editor "$SIMBOTIC_ROOT"/Simbotic.uproject
+SIMBOTIC_SETTINGS_PATH=$1
+
+"$SIMBOTIC_UE4"/Engine/Binaries/Linux/UE4Editor "$SIMBOTIC_ROOT"/Simbotic.uproject --airsim "$SIMBOTIC_SETTINGS_PATH"
